dc0701fea39124e5294cf7637a7e4475a1250788
Pepo-ro <taishin4649gogo@gmail.com>
2021-01-17 23:24:35 +0900

    set up mlflow storage

---
+++ IaC/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_storage_bucket" "ml_bucket" {
+  name          = var.bucket_name
+  location      = var.bucket_location
+  storage_class = var.storage_class
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+}
+
+resource "google_storage_bucket_object" "artifacts" {
+  name   = "artifacts/"
+  bucket = google_storage_bucket.ml_bucket.name
+}
+
+resource "google_storage_bucket_object" "mlruns" {
+  name   = "mlruns/"
+  bucket = google_storage_bucket.ml_bucket.name
+}
---
+++ IaC/variables.tf
@@ -0,0 +1,37 @@
+
+variable "project" {
+  type = string
+}
+
+variable "credentials_file" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+# GCS 設定
+variable "bucket_name" {
+  type = string
+}
+
+variable "bucket_location" {
+  type    = string
+  default = "US"
+}
+
+variable "storage_class" {
+  type    = string
+  default = "STANDARD"
+}
+
+variable "versioning_enabled" {
+  type    = bool
+  default = true
+}
+
