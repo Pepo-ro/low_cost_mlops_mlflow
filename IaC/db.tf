3bc8353f1802ac0884ac9b1185c2c3fee7656901
Pepo-ro <taishin4649gogo@gmail.com>
2021-02-21 18:16:08 +0900

    db設定を追加

---
+++ IaC/db.tf
@@ -0,0 +1,9 @@
+  
+module "db" {
+  source           = "./modules/postgresql"
+  database_version = var.database_version
+  region           = var.region
+  name             = "${var.project}-${var.dashboard_name}"
+  zone             = var.zone
+  project_id       = var.project
+}
