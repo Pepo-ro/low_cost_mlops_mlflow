779e5270c68368ee2e7748a66bbd8ceb4b799c8d
Pepo-ro <taishin4649gogo@gmail.com>
2021-02-21 18:38:03 +0900

    dbのresorce_nameを削除

--- IaC/db.tf
+++ IaC/db.tf
@@ -3,7 +3,6 @@ module "db" {
   db_name          = var.db_name
   database_version = var.database_version
   region           = var.region
-  name             = "${var.project}-${var.dashboard_name}"
   zone             = var.zone
   project_id       = var.project
 }
