8b2b15c29b23da70dbb7e63424186ca7bd8f77a9
Pepo-ro <taishin4649gogo@gmail.com>
2021-02-21 17:48:30 +0900

    事前設定ファイルに最終行を追加

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -12,26 +12,3 @@ provider "google" {
   project     = var.project
   region      = var.region
   zone        = var.zone
-}
-
-resource "google_storage_bucket" "ml_bucket" {
-  name          = var.bucket_name
-  location      = var.bucket_location
-  storage_class = var.storage_class
-
-  versioning {
-    enabled = var.versioning_enabled
-  }
-}
-
-resource "google_storage_bucket_object" "artifacts" {
-  name    = "artifacts/"
-  bucket  = google_storage_bucket.ml_bucket.name
-  content = var.content
-}
-
-resource "google_storage_bucket_object" "mlruns" {
-  name    = "mlruns/"
-  bucket  = google_storage_bucket.ml_bucket.name
-  content = var.content
-}
