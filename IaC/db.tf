878f92a5f9bc88393f3eaa25b99434fceae61c93
Pepo-ro <taishin4649gogo@gmail.com>
2021-03-14 16:46:33 +0900

    private network接続に修正

--- IaC/db.tf
+++ IaC/db.tf
@@ -2,12 +2,17 @@ module "db" {
   source           = "./modules/postgresql/modules/postgresql"
   db_name          = var.db_name
   database_version = var.database_version
-  name             = "${var.project}-dashboard-sample1"
+  name             = "${var.project}-dashboard"
   region           = var.region
   zone             = var.zone
   project_id       = var.project
-
-  ip_configuration = var.ip_configuration
-
+  user_name        = var.db_user_name
+  user_password    = var.db_password
+  ip_configuration = {
+    ipv4_enabled        = false
+    authorized_networks = []
+    require_ssl         = false
+    private_network     = google_compute_network.private_network.self_link
+  }
   module_depends_on = [google_service_networking_connection.private_vpc_connection.network]
 }
